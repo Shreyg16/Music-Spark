628f6226a57ea420d5ef18b8efbf28fa848f6ae8
Shreyg16 <shriyashgupta07@gmail.com>
2023-11-20 12:21:21 +0530

    first commit

---
+++ src/Components/Songs.js
@@ -0,0 +1,94 @@
+const Songs = [
+    {
+      id: 1,
+      favourite: false,
+      songName: "Bella Ciao",
+      artist: "El Profesor",
+      song: "https://open.spotify.com/album/05Ng00IfiS6xeyQX8WtjHU?si=LJD3oBktTHaOkrnUSTH4fA",
+      imgSrc:
+        "https://www.voicesofyouth.org/sites/voy/files/styles/big_blog_teaser/public/images/2020-04/money_heist.jpg?itok=03w4ZdKO",
+    },
+    {
+      id: 2,
+      favourite: false,
+      songName: "Perfect",
+      artist: "Ed Sheeran",
+      song: "https://open.spotify.com/track/0tgVpDi06FyKpA1z0VMD4v?si=aaa32dc4bd6346bf",
+      imgSrc:
+        "https://m.media-amazon.com/images/M/MV5BMzljNWVjODYtMjNkNS00N2E5LThlNmMtNjJiOTM1ZGVlM2NmXkEyXkFqcGdeQXVyMTk4OTA2Nzg@._V1_.jpg",
+    },
+    {
+      id: 3,
+      favourite: false,
+      songName: "Tum Se Hi",
+      artist: "Mohit Chauhan",
+      song: "https://open.spotify.com/track/7eQl3Yqv35ioqUfveKHitE?si=e68592cd5b5c41b3",
+      imgSrc:
+        "https://c.saavncdn.com/223/Jab-We-Met-Hindi-2007-20231016162009-500x500.jpg",
+    },
+    {
+      id: 4,
+      favourite: false,
+      songName: "Jalte Diye",
+      artist: "Vineet Singh",
+      song: "https://open.spotify.com/track/28Rpdw01WjsAKer8ZuEwo6?si=3ea37fcfef9e4e4c",
+      imgSrc:
+        "https://c.saavncdn.com/140/Prem-Ratan-Dhan-Payo-Hindi-2015-500x500.jpg",
+    },
+    {
+      id: 5,
+      favourite: false,
+      songName: "Apna Bana le",
+      artist: "Arjit Singh",
+      song: "https://open.spotify.com/track/5bQ6oDLqvw8tywmnSmwEyL?si=488d46532d4e434d",
+      imgSrc:
+        "https://www.ilyricshub.com/wp-content/uploads/2022/11/apna-bana-le-bhediya.jpg",
+    },
+    {
+      id: 6,
+      songName: "Dil Jhoom",
+      artist: "Arjit Singh",
+      song: "https://open.spotify.com/track/11qnwt18ZckPqchzK2I3U7?si=3b27f9e7859043c4",
+      imgSrc:
+        "https://c.saavncdn.com/247/Gadar-2-Hindi-2023-20230821100547-500x500.jpg",
+    },
+    {
+      id: 7,
+      favourite: false,
+      songName: "Waada Hai",
+      artist: "Arjun",
+      song: "https://open.spotify.com/track/39NIsABwAKQNjl4Mtqi0ME?si=728db3eb61b44d28",
+      imgSrc:
+        "https://a10.gaanacdn.com/images/albums/31/3504931/crop_480x480_3504931.jpg",
+    },
+    {
+      id: 8,
+      favourite: false,
+      songName: "Mahiye Jinna Sohna",
+      artist: "Darshan Rawal",
+      song: "https://open.spotify.com/track/2ncqKdTj6dz7tWoTMMrAtq?si=81ff98638a0a47f8",
+      imgSrc:
+        "https://c.saavncdn.com/122/Mahiye-Jinna-Sohna-Hindi-2023-20230801104702-500x500.jpg",
+    },
+    {
+      id: 9,
+      favourite: false,
+      songName: "Saudebazi",
+      artist: "Javid Ali",
+      song: "https://open.spotify.com/track/7lNbI1xd39QRdf6Mj8tvTv?si=61cef61c942c4bab",
+      imgSrc:
+        "https://c.saavncdn.com/427/Aakrosh-Hindi-2010-20221211135423-500x500.jpg",
+    },
+    {
+      id: 10,
+      favourite: false,
+      songName: "Tujhe Kitna Chahne Lage ",
+      artist: "Arjit Singh",
+      song: "https://open.spotify.com/track/2Fv2injs4qAm8mJBGaxVKU?si=5d346a032aed4235",
+      imgSrc:
+        "https://images.genius.com/a7b9543921e5d93edc8f11d5ae4e6ca4.939x939x1.jpg",
+    },
+  ];
+  
+  export { Songs };
+  
